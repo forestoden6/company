ae09642e8f7c18663824eb40a9100e2267891737
forestoden6 <forestoden6@gmail.com>
2018-08-23 13:38:54 -0600

    Removing tests

--- src/test/java/io/rscale/training/company/CompanyApplicationTests.java
+++ src/test/java/io/rscale/training/company/CompanyApplicationTests.java
@@ -9,8 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CompanyApplicationTests {
 
-	@Test
+	/*@Test
 	public void contextLoads() {
-	}
+
+	}*/
 
 }
